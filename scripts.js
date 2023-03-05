2681704351ab9777651dbfb2b5e613efbfca4bac
sawertsand22 <85335401+sawertsand22@users.noreply.github.com>
2023-03-05 23:49:39 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-var x = 100;
-q = document.getElementById(4).value;
+var x = 0;
+q = document.getElementById(4).innerText;
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -9,31 +9,48 @@ function getRandomInt(max) {
 function Ebal(){
  //   var shluha = (document.getElementById(1).value)
 //        alert(shluha)
-    document.getElementById(4).value=getRandomInt(1000);
-    suka=document.getElementById(4).value;
+    document.getElementById(4).innerText=getRandomInt(10000);
+    suka=document.getElementById(4).innerText;
     if(suka[1] == suka[2] && suka[0] == suka[1])
         //|| document.getElementById(4).value==111 || document.getElementById(4).value==222 || document.getElementById(4).value== 333 || document.getElementById(4).value==444  || document.getElementById(4).value==555 || document.getElementById(4).value==666 || document.getElementById(4).value==777 || document.getElementById(4).value==888 || document.getElementById(4).value==999)
     {
+
         x+=1600
-        document.getElementById(3).value='Balance: '+ x;
-        alert('You win')
+        document.getElementById(3).innerText='Balance: '+ x;
+        alert('You win');
+
     }
     else if  (suka[0]==suka[1])
     {
         x+=450
-        document.getElementById(3).value='Balance: '+x;
-        alert('You win')
+        document.getElementById(3).innerText='Balance: '+x;
+
+        alert('You win');
     }
+
+
+
+    else if  (suka[0]==suka[1]&& suka[1]==suka[2] && suka[2]==suka[3])
+    {
+        x+=2150
+        document.getElementById(3).innerText='Balance: '+x;
+
+        alert('You win');
+    }
+
+
+
+
 else {
         x -= 10;
-        document.getElementById(3).value = 'Balance: ' + x;
+        document.getElementById(3).innerText = 'Balance: ' + x;
     }
 if(x<=-10)
 {
     alert('You are fucking looser')
     alert('shut up and take my money')
-    x=100;
-    document.getElementById(3).value = 'Balance: ' + x;
+    x=10;
+    document.getElementById(3).innerText = 'Balance: ' + x;
 }
 
 }
