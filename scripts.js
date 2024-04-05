8e7a3db2fd3bd6288e5b3e01fcc1f1ac2016ff92
sawertsand22 <85335401+sawertsand22@users.noreply.github.com>
2024-04-05 12:06:47 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 var x = 100;
-q = document.getElementById(5).value;
-let stavka=Number(document.getElementById(5).innerText);
+// = document.getElementById(5).value;
+let stavka;
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 
 function Ebal(){
-    avka=Number(document.getElementById(5).innerText);
+    stavka==Number(document.getElementById(5).value);
  //   var shluha = (document.getElementById(1).value)
 //        alert(shluha)
     document.getElementById(4).innerText=getRandomInt(10000);
-    suka=document.getElementById(4).innerText;
+    suka=document.getElementById(4).value;
     if(suka[1] == suka[2] && suka[0] == suka[1])
         //|| document.getElementById(4).value==111 || document.getElementById(4).value==222 || document.getElementById(4).value== 333 || document.getElementById(4).value==444  || document.getElementById(4).value==555 || document.getElementById(4).value==666 || document.getElementById(4).value==777 || document.getElementById(4).value==888 || document.getElementById(4).value==999)
     {
